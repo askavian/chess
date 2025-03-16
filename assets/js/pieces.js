96b8355947b5a7cda7f5600e6fce46f979f8e8fb
askavian <malte.boettcher@live.com>
2025-03-16 21:21:41 +0100

    feat: chghess pieces assigned in js

---
+++ assets/js/pieces.js
@@ -0,0 +1,51 @@
+import dark_bishop_rotated from '../pieces/dark_bishop_45_rotated.svg';
+import dark_bishop from '../pieces/dark_bishop_45.svg';
+import dark_king_rotated from '../pieces/dark_king_45_rotated.svg';
+import dark_king from '../pieces/dark_king_45.svg';
+import dark_knight_rotated from '../pieces/dark_knight_45_rotated.svg';
+import dark_knight from '../pieces/dark_knight_45_.svg';
+import dark_pawn_rotated from '../pieces/dark_pawn_45_rotated.svg';
+import dark_pawn from '../pieces/dark_pawn_45.svg';
+import dark_queen_rotated from '../pieces/dark_queen_45_rotated.svg';
+import dark_queen from '../pieces/dark_queen_45.svg';
+import dark_rook_rotated from '../pieces/dark_rook_45_rotated.svg';
+import dark_rook from '../pieces/dark_rook_45_.svg';
+import white_bishop_rotated from '../pieces/white_bishop_45_rotated.svg';
+import white_bishop from '../pieces/white_bishop_45.svg';
+import white_king_rotated from '../pieces/white_king_45_rotated.svg';
+import white_king from '../pieces/white_king_45.svg';
+import white_knight_rotated from '../pieces/white_knight_45_rotated.svg';
+import white_knight from '../pieces/white_knight_45_.svg';
+import white_pawn_rotated from '../pieces/white_pawn_45_rotated.svg';
+import white_pawn from '../pieces/white_pawn_45.svg';
+import white_queen_rotated from '../pieces/white_queen_45_rotated.svg';
+import white_queen from '../pieces/white_queen_45.svg';
+import white_rook_rotated from '../pieces/white_rook_45_rotated.svg';
+import white_rook from '../pieces/white_rook_45_.svg';
+
+const image = [
+    dark_bishop_rotated,
+    dark_bishop,
+    dark_king_rotated,
+    dark_king,
+    dark_knight_rotated,
+    dark_knight,
+    dark_pawn_rotated,
+    dark_pawn,
+    dark_queen_rotated,
+    dark_queen,
+    dark_rook_rotated,
+    dark_rook,
+    white_bishop_rotated,
+    white_bishop,
+    white_king_rotated,
+    white_king,
+    white_knight_rotated,
+    white_knight,
+    white_pawn_rotated,
+    white_pawn,
+    white_queen_rotated,
+    white_queen,
+    white_rook_rotated,
+    white_rook
+]
